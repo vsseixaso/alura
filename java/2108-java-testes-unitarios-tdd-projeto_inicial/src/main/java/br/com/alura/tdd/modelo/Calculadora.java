46bce18a867c977ef069bd6fb3e0ab09db08eade
Vinicius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2021-11-26 23:23:49 -0300

    class 01 completed

--- java/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Calculadora.java
+++ java/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Calculadora.java
@@ -1,2 +1,8 @@
-package br.com.alura.tdd.modelo;public class Calculadora {
+package br.com.alura.tdd.modelo;
+
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
 }
