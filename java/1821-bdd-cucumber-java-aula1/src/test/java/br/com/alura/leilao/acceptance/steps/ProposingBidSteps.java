891340bbb6692fc4e5db78b250a5e9f5408b688c
Vinicius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2021-12-12 17:19:31 -0300

    class 04 completed

--- java/1821-bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/acceptance/steps/ProposingBidSteps.java
+++ java/1821-bdd-cucumber-java-aula1/src/test/java/br/com/alura/leilao/acceptance/steps/ProposingBidSteps.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -12,6 +13,8 @@ import org.junit.Assert;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ProposingBidSteps {
 
@@ -27,7 +30,6 @@ public class ProposingBidSteps {
 
     @After
     public void tearDown() {
-        System.out.println("@After");
     }
 
     @Given("a valid bid")
@@ -47,9 +49,9 @@ public class ProposingBidSteps {
         Assert.assertEquals(BigDecimal.TEN, auction.getLances().get(0).getValor());
     }
 
-//    @Given("a valid bid of (\\d+[.]\\d\\d?) reais from the user (\\w+)$")
-    @Given("a valid bid of {double} reais from the user {string}")
-    public void a_valid_bid_of_reais_from_the_user(Double value, String username) {
+//    @Given("a valid bid of (\\d+[.]\\d\\d?) BRL from the user (\\w+)$")
+    @Given("a valid bid of {double} BRL from the user {string}")
+    public void a_valid_bid_of_brl_from_the_user(Double value, String username) {
         Lance bid = new Lance(new Usuario(username), new BigDecimal(value));
         bids.add(bid);
     }
@@ -64,5 +66,33 @@ public class ProposingBidSteps {
         Assert.assertEquals(bids.size(), auction.getLances().size());
         Assert.assertEquals(bids.get(0).getValor(), auction.getLances().get(0).getValor());
         Assert.assertEquals(bids.get(1).getValor(), auction.getLances().get(1).getValor());
+    }
+
+    @Given("a invalid bid of {double} BRL from the user {string}")
+    public void a_invalid_bid_of_brl_from_the_user(Double value, String username) {
+        bid = new Lance(new BigDecimal(value));
+    }
+
+    @Then("bid is denied")
+    public void bid_is_denied() {
+        Assert.assertEquals(0, auction.getLances().size());
+    }
+
+    @Given("two bids")
+    public void two_bids(DataTable dataTable) {
+        List<Map<String, String>> values = dataTable.asMaps();
+        values.forEach(map -> {
+            String value = map.get("value");
+            String username = map.get("username");
+
+            Lance bid = new Lance(new Usuario(username), new BigDecimal(value));
+            bids.add(bid);
+        });
+    }
+
+    @Then("the second bid is denied")
+    public void the_second_bid_is_denied() {
+        Assert.assertEquals(1, auction.getLances().size());
+        Assert.assertEquals(bids.get(0).getValor(), auction.getLances().get(0).getValor());
     }
 }
--- java/1821-bdd-cucumber-java-aula1/src/test/resources/features/proposing_bids.feature
+++ java/1821-bdd-cucumber-java-aula1/src/test/resources/features/proposing_bids.feature
@@ -6,7 +6,25 @@ Feature: Proposing Bids to Auction
     Then bid is accepted
 
   Scenario: Proposing multiple valid bids
-    Given a valid bid of 10.0 reais from the user "fulano"
-      And a valid bid of 15.0 reais from the user "beltrano"
+    Given a valid bid of 10.0 BRL from the user "fulano"
+      And a valid bid of 15.0 BRL from the user "beltrano"
     When propose multiple bids to auction
     Then bids are accepted
+
+  Scenario Outline: Proposing a invalid bid
+    Given a invalid bid of <value> BRL from the user '<username>'
+    When propose to auction
+    Then bid is denied
+
+    Examples:
+      | value | username |
+      |     0 | beltrano |
+      |    -1 | ciclano  |
+
+  Scenario: Proposing a sequence of bids
+    Given two bids
+      | value | username |
+      |  10.0 | beltrano |
+      |  15.0 | beltrano  |
+    When propose multiple bids to auction
+    Then the second bid is denied
